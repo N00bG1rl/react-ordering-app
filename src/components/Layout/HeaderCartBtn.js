153507998c424e4a238594493c2b970c23912468
N00bG1rl <marttila.helen@gmail.com>
2021-08-19 12:00:49 +0300

    CartReducer

--- src/components/Layout/HeaderCartBtn.js
+++ src/components/Layout/HeaderCartBtn.js
@@ -1,8 +1,20 @@
+import { useContext } from "react"
+
 import CartIcon from "../Cart/CartIcon"
+import CartContext from "../../store/cart-context"
 
 import styles from "./HeaderCartBtn.module.css"
 
 const HeaderCartBtn = props => {
+  // For shoping cart update
+  const cartCtx = useContext(CartContext)
+
+  // Reduce helps to limit to 1 number, it takes two arguments - a function
+  // and a starting value 0
+  const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+    return curNumber + item.amount
+  }, 0)
+
   return (
     /* onClick here is build in and uses header.js name choice */
     <button className={styles.button} onClick={props.onClick}>
@@ -10,7 +22,7 @@ const HeaderCartBtn = props => {
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={styles.badge}>3</span>
+      <span className={styles.badge}>{numberOfCartItems}</span>
     </button>
   )
 }
--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,19 +1,60 @@
+// For managing state. Check if shoping bag allready have that
+// item and
+import { useReducer } from "react"
+
 import CartContext from "./cart-context"
 
+const defaultCartState = {
+  items: [],
+  totalAmount: 0,
+}
+
+// Outside of main comp bec. it does not need anything from inside
+const cartReducer = (state, action) => {
+  if (action.type === "ADD_CART_ITEM") {
+    const updatedItems = state.items.concat(action.item)
+    // Check same product amount and add price
+    const updatedTotalAmount =
+      state.totalAmount + action.item.price * action.item.amount
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount,
+    }
+  }
+  return defaultCartState
+}
+
 const CartProvider = props => {
-  const handleAddItemToCart = item => {}
+  // Using reducer.
+  // First arg pointing to reducer func.. (dont execute, just point)
+  // Sec arg is inital state
+  // Reducer returns array of two elements. So we use array destructurin
+  // to pull these elements out of that array and store them at seperate
+  // constant
+  // The first array element is state snapshot 'cartState', sec is func
+  // whitch allows dispatch an action to the reducer
+  const [cartState, dispatchCartAction] = useReducer(
+    cartReducer,
+    defaultCartState
+  )
 
-  const handleRemoveItemFromCart = item => {}
+  const handleAddItemToCart = item => {
+    dispatchCartAction({ type: "ADD_CART_ITEM", item: item })
+  }
 
-  const CartContext = {
-    items: [],
-    totalAmount: 0,
+  const handleRemoveItemFromCart = id => {
+    dispatchCartAction({ type: "REMOVE_CART_ITEM", id: id })
+  }
+
+  const cartContext = {
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
     addItem: handleAddItemToCart,
     removeItem: handleRemoveItemFromCart,
   }
 
   return (
-    <CartContext.Provider value={CartContext}>
+    <CartContext.Provider value={cartContext}>
       {props.children}
     </CartContext.Provider>
   )
